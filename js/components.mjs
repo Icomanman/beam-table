6fea0aa5ab45976955001ab5db0c08a7f60a7bcf
Icomanman <fred_barabs@yahoo.com>
2021-10-14 23:11:42 +0800

    input and UI data extraction and validations are in place; to work on results data structure.

--- js/components.mjs
+++ js/components.mjs
@@ -1,6 +1,43 @@
 
 import { calcBending } from "./app.mjs";
 
+const checkFields = () => {
+    let has_err = false;
+    jQuery('td').each(function () {
+        const input = jQuery(this).find('input');
+        if (input.length == 0) return;
+        const dropdown = jQuery(this).find('.ui.dropdown');
+        if (input.val() === '') {
+            jQuery(this).addClass('error');
+            dropdown.addClass('error');
+            has_err = true;
+        }
+    });
+    return has_err;
+};
+
+const checkProps = () => {
+    let has_err = false;
+    // extract and validate (additional check) data from props:
+    const DAT = ACI.UI.data;
+    const fields = Object.keys(DAT);
+    if (fields.length < 4) return true;
+    else {
+        var nans = fields.filter(field => {
+            DAT[field] = parseFloat(DAT[field]);
+            return isNaN(DAT[field]);
+        });
+    }
+    if (nans.length > 0) has_err = true;
+    return has_err;
+}
+
+const removeErrorClass = id => {
+    const td = jQuery(`#${id}`);
+    td.removeClass('error');
+    td.find('.ui.dropdown').removeClass('error');
+};
+
 const tableRow = () => {
     const comp_options = {
         data: function () {
@@ -9,12 +46,17 @@ const tableRow = () => {
                 depths: [300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800],
                 widths: [250, 300, 350, 400, 450, 500, 550, 600],
                 b_arr: [''],
-                h_arr: ['']
+                h_arr: [''],
+                results: []
             }
         },
         methods: {
-            updateValue: function (evt, key) {
-                calcBending();
+            removeErr: function (id) {
+                removeErrorClass(id);
+            },
+            runCalc: function (dat) {
+                if (!checkFields() && !checkProps()) calcBending();
+                // else alert('err');
             }
         },
         mounted: function () {
@@ -39,6 +81,8 @@ const tableRow = () => {
                         jQuery(`#b-selection-${last_no + 1}`).dropdown();
                         jQuery(`#h-selection-${last_no + 1}`).dropdown();
                     });
+                    (this.is_b_empty).push(false);
+                    (this.is_h_empty).push(false);
                 }
             });
             ACI.v_EVENT.$on('delete_row', dat => {
@@ -46,25 +90,28 @@ const tableRow = () => {
                 (this.b_arr).pop();
                 (this.h_arr).pop();
             });
+            ACI.v_EVENT.$on('run', dat => {
+                this.runCalc();
+            });
         },
         props: {
             passed: Object
         },
         template: `
         <tbody>
-            <tr v-for="(row, i) in table_rows" class="center aligned">
-                <td>
+            <tr v-for="(row, i) in table_rows" class="center aligned" :key="row">
+                <td :id="'td-b' + i">
                     <div :id="'b-selection-' + (i + 1)" class="ui inline dropdown">
-                        <input type="hidden" :name="'b' + (i + 1)" v-model.lazy="b_arr[i + 1]">
+                        <input type="hidden" :name="'b' + row" v-model.lazy="b_arr[i]" @change="removeErr('td-b' + i)">
                         <div class="default text">Select</div>
                         <div class="menu">
                             <div class="item" v-for="width in widths" :data-value="width">{{width}}</div>
                         </div>
                      </div>
                 </td>
-                <td>
+                <td :id="'td-h' + i">
                     <div :id="'h-selection-' + (i + 1)" class="ui inline dropdown">
-                        <input type="hidden" :name="'h' + (i + 1)" v-model.lazy="h_arr[i + 1]">
+                        <input type="hidden" :name="'h' + row" v-model.lazy="h_arr[i]" @change="removeErr('td-h' + i)">
                         <div class="default text">Select</div>
                         <div class="menu">
                             <div class="item" v-for="depth in depths" :data-value="depth">{{depth}}</div>
